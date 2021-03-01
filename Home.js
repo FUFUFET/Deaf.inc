c30464a77c1b7f2d25d9d1863ff2847733ddf12f
FUFUFET <70544989+FUFUFET@users.noreply.github.com>
2021-02-28 23:00:43 -0500

    animation on/off

---
+++ Home.js
@@ -0,0 +1,13 @@
+let a;
+function show_hide()
+{
+  if(a==1)
+  {document.getElementById("fireofring").style.display="inline";
+  return a=0;
+}
+
+else
+  {document.getElementById("fireofring").style.display="none";
+  return a=1;
+}
+}
